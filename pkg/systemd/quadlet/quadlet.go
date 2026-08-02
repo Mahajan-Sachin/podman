e822e89e5d86b9f39334efc4e22d9c47c9f693fe
Sachin Mahajan <sachinmahajan903@gmail.com>
2026-08-02 22:15:45 +0530

    quadlet: do not set RemainAfterExit=yes for volume units by default
    Do not set RemainAfterExit=yes by default when converting Quadlet
    .volume files to systemd oneshot services.
    
    Setting RemainAfterExit=yes causes systemd to consider the volume
    service active even after the volume is deleted manually, preventing
    systemd from re-executing volume creation when dependent container
    services restart.
    
    Fixes #27862
    
    Signed-off-by: Sachin Mahajan <sachinmahajan903@gmail.com>

--- pkg/systemd/quadlet/quadlet.go
+++ pkg/systemd/quadlet/quadlet.go
@@ -1220,7 +1220,7 @@ func ConvertVolume(volume *parser.UnitFile, unitsInfoMap map[string]*UnitInfo, i
 
 	service.AddCmdline(ServiceGroup, "ExecStart", podman.Args)
 
-	defaultOneshotServiceGroup(service, true)
+	defaultOneshotServiceGroup(service, false)
 
 	// Store the name of the created resource
 	unitInfo.ResourceName = volumeName
